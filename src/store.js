ad708e167dfe000efa3603463b16438a21052d22
sam king <samrk314@gmail.com>
2020-12-27 08:09:24 -0500

    adding @redux/toolkit and rewriting store file

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { applyMiddleware, createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import { createLogger } from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import { promiseMiddleware, localStorageMiddleware } from './middleware';
@@ -14,12 +15,15 @@ const myRouterMiddleware = routerMiddleware(history);
 
 const getMiddleware = () => {
   if (process.env.NODE_ENV === 'production') {
-    return applyMiddleware(myRouterMiddleware, promiseMiddleware, localStorageMiddleware);
+    return [myRouterMiddleware, promiseMiddleware, localStorageMiddleware];
   } else {
     // Enable additional logging in non-production environments.
-    return applyMiddleware(myRouterMiddleware, promiseMiddleware, localStorageMiddleware, createLogger())
+    return [myRouterMiddleware, promiseMiddleware, localStorageMiddleware, createLogger()];
   }
 };
 
-export const store = createStore(
-  reducer, composeWithDevTools(getMiddleware()));
+export const store = configureStore({
+	reducer: reducer,
+	middleware: getMiddleware(),
+	devTools: true
+})
