1ff2aa1ce8a76a8383521c23d515094f940e06a3
sam king <samrk314@gmail.com>
2020-12-28 17:28:19 -0500

    cleaning up

--- src/helpers/convert.sh
+++ src/helpers/convert.sh
@@ -4,8 +4,6 @@ output="./src/actions.index.js"
 while read -r line || [[ -n "$line" ]]
 do
   var1=$(echo $line | cut -f2 -d\')
-  echo "$var1,"
   var2=$(echo "${var1//_}")
   var3=$(echo "$var2" | tr '[:upper:]' '[:lower:]')
-  echo "export const $var3 = createAction($var1);" >> output
 done < "$input"
